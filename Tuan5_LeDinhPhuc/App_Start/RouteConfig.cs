c3849554e03ea66566fe8b2f689ed8c28d52904e
Đình Phúc <38375962+dinhphuc1309@users.noreply.github.com>
2022-06-16 15:32:25 +0700

    xong lab3

--- Tuan5_LeDinhPhuc/App_Start/RouteConfig.cs
+++ Tuan5_LeDinhPhuc/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Tuan5_LeDinhPhuc
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Sach", action = "ListSach", id = UrlParameter.Optional }
             );
         }
     }
