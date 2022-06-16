c11118c2b6062ccce0d589271d8b44ce569c6094
Đình Phúc <38375962+dinhphuc1309@users.noreply.github.com>
2022-06-16 20:18:41 +0700

    xong lab 4

---
+++ Tuan5_LeDinhPhuc/Controllers/NguoiDungController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tuan5_LeDinhPhuc.Models;
+
+namespace Tuan5_LeDinhPhuc.Controllers
+{
+    public class NguoiDungController : Controller
+    {
+        // GET: NguoiDung
+        MyDataDataContext data = new MyDataDataContext();
+        [HttpGet]
+        public ActionResult DangKy()
+        {
+            return View();
+        }
+    }
+}
